726ed4eec055fe6046780caeec1adbe131428f91
hellogaon <la_vega_@naver.com>
2018-07-28 22:54:08 +0900

    Update gcd-lcm.cpp

--- 기본 기법/gcd-lcm.cpp
+++ 기본 기법/gcd-lcm.cpp
@@ -7,3 +7,16 @@ int gcd(int a, int b){
 int lcm(int a, int b){
   return a*b/gcd(a,b);
 }
+
+//확장 유클리드 호제법
+//as + bt = gcd(a,b)에서의 s,t 값을 구한다.
+int extended_gcd(int a, int b, int &s, int &t){
+  if(!b){
+    s = 1, t = 0;
+    return a;
+  }
+  int q = a/b, r = a%b, sp, tp;
+  int g = ee(b,r,sp,tp);
+  s = tp; t = sp-tp*q;
+  return g;
+}
