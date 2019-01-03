d5dca5425453046bb0a60267f332665f2689f0a1
hellogaon <la_vega_@naver.com>
2019-01-04 04:01:52 +0900

    Update TL

---
+++ TL.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define mp make_pair
+#define pb push_back
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> pii;
+const int INF = 987654321;
+const int MOD = 1000000007;
+
+int main(){
+  
+}
