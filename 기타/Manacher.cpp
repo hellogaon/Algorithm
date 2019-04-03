66a7d6b07b6680204b70d85edcfeb2db6a9c8c81
hellogaon <la_vega_@naver.com>
2019-04-03 22:46:33 +0900

    Update Manacher.cpp

--- 기타/Manacher.cpp
+++ 기타/Manacher.cpp
@@ -1,20 +1,18 @@
 //Manacher - 펠린드롬 판별 알고리즘 : O(N)
 const int MAXN = 2000001;
 
-int pal[MAXN*2+1];
+int S[MAXN*2+1], pal[MAXN*2+1];
 
-void manacher(char *str){
-  char s[MAXN*2+1] = {};
-  int len = strlen(str);
-  for(int i=0;i<len;i++){
-    s[i*2] = str[i];
-    if(i != len-1) s[i*2+1] = '#';
+void manacher(int len){
+  len = len*2+1;
+  for(int i=len-1;i>=0;i--){
+    if(i % 2 == 0) S[i] = -1;
+    else S[i] = S[i/2];
   }
-  int c = 0, r = 0; len = strlen(s);
+  int r = 0, p = 0;
   for(int i=0;i<len;i++){
-    int mirr = i-2*(i-c);
-    if(i < r) pal[i] = min(r-i,pal[mirr]);
-    while(i-pal[i]>0 && i+pal[i]-1<len && s[i-pal[i]-1]==s[i+pal[i]+1]) pal[i]++;
-    if(pal[i]+i > r) r = pal[i]+i, c = i;
+    if(i <= r) pal[i] = min(pal[2*p-i],r-i);
+    while(i-pal[i]>0 && i+pal[i]+1<len && S[i-pal[i]-1]==S[i+pal[i]+1]) pal[i]++;
+    if(r < i+pal[i]) r = i+pal[i], p = i;
   }
 }
